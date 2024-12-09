e57cce85eddb66a1c7cd352ee144ac619af9cc1c
Tadakuniii <raita2005@icloud.com>
2024-12-09 14:03:38 +0900

    aaa

---
+++ pages/test.vue
@@ -0,0 +1,85 @@
+<div class="container">
+  <h1 class="display-1">美味しさNo.１！KFC風フライドチキン</h1>
+</div>
+
+<div class="container">
+  <p class="h2 m-5">KFC人気レシピ1位！数あるKFCレシピ─どれも何か一味足りない気が。試行錯誤の末の主人も絶賛☆再現率の高いレシピです☆</p>
+</div>
+
+<div class="container">
+  <div class="row">
+
+    <div class="col-12 col-md-4 p-auto border border-primary">
+      <h2 class="text-center">材料</h2>
+      <ul>
+        <li class="mark">鶏肉(お好きな部位)　２〜３枚位</li>
+        <li>☆牛乳　1/2カップ</li>
+        <li>☆卵　１個</li>
+        <li>☆にんにくチューブ　２センチ位</li>
+        <li>★薄力粉　１カップ</li>
+        <li>★オールスパイス　小さじ１弱</li>
+        <li>★顆粒コンソメ　大さじ1/2</li>
+        <li>★塩　大さじ1/2</li>
+        <li>あれば★パプリカパウダー　小さじ１</li>
+      </ul>
+    </div>
+    <div class="col-12 col-md-4 px-5 border border-success">
+      <h2 class="text-center">作り方</h2>
+      <ol>
+        <li>☆をボウルに混ぜ合わせておく</li>
+        <li>ボウルに★を全て混ぜ合わせておく。</li>
+        <li>肉の厚みがある部分を切り開き、厚みを一定にする。鶏肉１枚を２～３等分の大きめに切る。</li>
+        <li>１のボウルに鶏肉を浸す。(時間があれば20分以上浸す。)</li>
+        <li>２のボウルに鶏肉を入れて粉類をまんべんなくまぶし、別の容器に並べていく。粉が余るようでしたら、二度付けをお勧めします！</li>
+        <li>卵液→粉→卵液→粉と二度付けする事で、カリっと感がアップ＋味もしっかり付きます！写真の我が家のは二度付けしています。</li>
+        <li>油を160度に温める。(温めるのはこのタイミングで大丈夫です。待ってる間に衣が鶏肉になじみます。)</li>
+        <li>160度で５分以上、様子を見ながら、火が通ったら、一度上げて、今度は180～200度でカリっと仕上げ、完成です☆</li>
+        <li>鶏肉の1枚はサイズにばらつきがある為、2～3枚という記載に変更しました。</li>
+      </ol>
+    </div>
+
+    <div class="col-12 col-md-4 py-5 border border-danger">
+      <h2 class="text-center">コツ・ポイント</h2>
+      <p><del>揚げる際鍋底での焦げ防止に衣が軽く揚がるまで菜箸等で時々浮かせて下さい</del></p>
+      <p><ins>好みで粉類に粗びき黒胡椒<mark>小さじ1/2</mark>を足しても◎</ins></p>
+      <p><strong>にんにくチューブ２センチはガーリックパウダー小さじ１に変えて粉類と混ぜても美味しいです。</strong></p>
+      <p><em>パプリカパウダーは無くても可</em></p>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <h2 class="text-center mt-5">このレシピの生い立ち</h2>
+
+  今まで作ったKFCレシピは、卵液や粉もかなり余るし、何よりも
+  <p>何かひと味足りない……と思って、<small class="text-body-secondary">やっとKFC味にたどり着きました！</small></p>
+  <p>どの家庭にもあるような材料で☆</p>
+  <p class="lead">牛乳と卵液、粉類も必要量のみになっているので無駄が無く経済的です☆</p>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+  <label class="form-check-label" for="flexCheckDefault">
+    KFC好き
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+  <label class="form-check-label" for="flexCheckChecked">
+    KFC嫌い
+  </label>
+</div>
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">メールアドレス</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">あなたのEメールを他者と共有することはありません。</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">パスワード</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">チェックしてください</label>
+  </div>
+  <button type="submit" class="btn btn-primary">投稿する</button>
+</form>
